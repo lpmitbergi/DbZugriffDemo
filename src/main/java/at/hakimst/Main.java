93e5e7935c3a316743bec9008336b8d6643a7c13
Markus Grün <margruen@tsn.at>
2024-11-12 11:30:40 +0100

    SQL Code der Datenbank wurde hier als Kommentar "gespeichert"

--- src/main/java/at/hakimst/Main.java
+++ src/main/java/at/hakimst/Main.java
@@ -4,13 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, 'Markus Grün', 'margruen@myimst.at'), (NULL, 'Maria Koenne', 'mariakoenne@myimst.at');
+
 public class Main {
     public static void main(String[] args) {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
         try{
 
-            Connection con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root","");
+            Connection con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jbcdemo","root","");
 
         }catch (SQLException e){
 
