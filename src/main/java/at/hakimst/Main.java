b326fd4a66e3e3b3cfa9b2677d77ff750290957b
Markus Grün <margruen@tsn.at>
2024-11-18 08:11:59 +0100

    Video 8 fertig gemacht und die Methode findAllByNameLike in den Code eingebunden

--- src/main/java/at/hakimst/Main.java
+++ src/main/java/at/hakimst/Main.java
@@ -14,6 +14,8 @@ public class Main {
         updateStudentDemo(6,"Neuer Name", "neueEmail@provider.at");
         selectAllDemo();
         deleteStudentDemo(7);
+        selectAllDemo();
+        findAllByNameLike("zeck");
 //        try {
 
 //            Class.forName("com.mysql.cj.jdbc.Driver");
@@ -37,6 +39,39 @@ public class Main {
 //        }
 
     }
+
+    private static void findAllByNameLike(String pattern) {
+        System.out.println("Find all by Name DEMO mit JDBC");
+
+        String connectionUrl = "jdbc:mysql://127.0.0.1:3306/jbcdemo";
+
+        String user = "root";
+
+        String pwd = "";
+
+        try (Connection conn = DriverManager.getConnection(connectionUrl, user, pwd);) {
+
+            System.out.println("Verbindung zur DB hergestellt!");
+
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM `student` WHERE `student`.`name` LIKE ?");
+            preparedStatement.setString(1,"%"+pattern+"%");
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next())
+            {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                System.out.println("Student aus der DB: [ID] " + id + " [NAME]" + name + " [EMAIL]" + email);
+            }
+
+        } catch (SQLException e) {
+
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: " + e.getMessage());
+
+        }
+
+    }
+
 
     public static void deleteStudentDemo(int studentId)
     {
