2733b1a44bd9b3ff64897bd1635a962eb0db8fcb
Markus Grün <margruen@tsn.at>
2024-11-18 09:25:56 +0100

    insertMitschueler implementiert und getestet

--- src/main/java/at/hakimst/App.java
+++ src/main/java/at/hakimst/App.java
@@ -4,7 +4,40 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) {
+        insertMitschueler("Markus","PS5 spielen");
+    }
+    public static void insertMitschueler(String mitschueler, String hobby)
+    {
+        System.out.println("INSERT Mitschueler mit JDBC");
 
+        String connectionUrl = "jdbc:mysql://127.0.0.1:3306/jbcdemo";
+
+        String user = "root";
+
+        String pwd = "";
+
+        try (Connection conn = DriverManager.getConnection(connectionUrl, user, pwd);) {
+
+            System.out.println("Verbindung zur DB hergestellt!");
+
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "INSERT INTO `hobbies` (`id`, `mitschueler`, `hobby`) VALUES (NULL, ?, ?)");
+            try
+            {
+                preparedStatement.setString(1,mitschueler);
+                preparedStatement.setString(2,hobby);
+                int rowAffected =  preparedStatement.executeUpdate();
+                System.out.println(rowAffected + "Datensätze eingefügt");
+            } catch (SQLException ex)
+            {
+                System.out.println("Fehler im SQL-INSERT Statement: " + ex.getMessage());
+            }
+
+        } catch (SQLException e) {
+
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: " + e.getMessage());
+
+        }
     }
     public static void selectAllMitschueler() {
 
